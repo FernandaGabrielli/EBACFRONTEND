5a8bfb295251272c395da3390df2d8029fe6b2bc
FernandaGabrielli <123499117+FernandaGabrielli@users.noreply.github.com>
2023-08-08 17:09:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ $(document).ready(function() {
     $('#lista-tarefas').submit(function(e) {
         e.preventDefault();
 
-        const inputNomeDaTarefa = $('#Nome-Da-Tarefa').val();
+        const inputNomeDaTarefa = $('NomeDaTarefa').val();
 
         if (inputNomeDaTarefa) {
             const novaTarefa = `<li>${inputNomeDaTarefa}</li>`;
             listaTarefas.append(novaTarefa);
 
             // Limpa o campo de entrada
-            $('#Nome-Da-Tarefa').val('');
+            $('NomeDaTarefa').val('');
 
             // Adiciona o evento de clicar para aplicar a classe 'finalizada'
             $('li').click(function() {
