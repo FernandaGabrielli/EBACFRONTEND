62aaaded8e85e258d351e9e559cbcb70ea7add1e
FernandaGabrielli <123499117+FernandaGabrielli@users.noreply.github.com>
2023-08-08 17:14:48 -0300

    Update main.js
    correcao de erro

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
-// main.js
 $(document).ready(function() {
     const listaTarefas = $('ul');
 
     $('#lista-tarefas').submit(function(e) {
         e.preventDefault();
 
-        const inputNomeDaTarefa = $('NomeDaTarefa').val();
+        const inputNomeDaTarefa = $('#NomeDaTarefa').val();  // Adicione o # aqui
 
         if (inputNomeDaTarefa) {
             const novaTarefa = `<li>${inputNomeDaTarefa}</li>`;
             listaTarefas.append(novaTarefa);
 
             // Limpa o campo de entrada
-            $('NomeDaTarefa').val('');
-
-            // Adiciona o evento de clicar para aplicar a classe 'finalizada'
-            $('li').click(function() {
-                $(this).toggleClass('finalizada');
-            });
+            $('#NomeDaTarefa').val('');  // Adicione o # aqui
         }
+    });
+
+    // Adiciona o evento de clicar para aplicar a classe 'finalizada'
+    $('ul').on('click', 'li', function() {
+        $(this).toggleClass('finalizada');
     });
 });
